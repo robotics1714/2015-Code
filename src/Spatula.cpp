a6d656ef295feba7ebdbc7f136f97109847e3240
MORE Robotics Team 1714 <robotics1714@gmail.com>
2015-01-18 10:32:44 -0600

    Revert "Added function to Spatula Class"
    This reverts commit 9e42b776247e3684521250d5cc30babddc2d406c.

--- src/Spatula.cpp
+++ src/Spatula.cpp
@@ -7,18 +7,14 @@
 
 #include "Spatula.h"
 
-Spatula::Spatula(int talonDeviceNumber, int potPort, string name) : MORESubsystem(name)
+Spatula::Spatula(int talonDeviceNumber, string name) : MORESubsystem(name)
 {
 	rotaryMotor = new CANTalon(talonDeviceNumber);
-	pot = new AnalogInput(potPort);
-	movingUp=false;
-	movingDown=false;
 }
 
 Spatula::~Spatula()
 {
 	delete rotaryMotor;
-	delete pot;
 }
 
 ///Set Motor Speed
@@ -26,62 +22,6 @@ Spatula::~Spatula()
 void Spatula::Rotate(float speed)
 {
 	rotaryMotor->Set(speed);
-}
-
-void Spatula::StartMoveUp()
-{
-	if((!movingUp)&&(!movingDown))
-	{
-		movingUp=true;
-	}
-}
-
-void Spatula::StartMoveDown()
-{
-	if((!movingUp)&&(!movingDown))
-	{
-		movingDown=true;
-	}
-}
-
-bool Spatula::MoveUp()
-{
-	///Check if motor is moving up
-	if(movingUp)
-	{
-		///Move up if the spatula isn't up already
-		if(pot->GetAverageValue() < SPATULA_UP)
-		{
-			rotaryMotor->Set(CURVE_IN);
-		}
-		///Set Motor to 0
-		else
-		{
-			rotaryMotor->Set(0);
-			movingUp=false;
-		}
-	}
-	return movingUp;
-}
-
-bool Spatula::MoveDown()
-{
-	///Check if Motor is moving down
-	if(movingDown)
-	{
-		///Move down if the spatula isn't moving
-		if(pot->GetAverageValue() < SPATULA_UP)
-		{
-			rotaryMotor->Set(CURVE_OUT);
-		}
-		///Set Motor to 0
-		else
-		{
-			rotaryMotor->Set(0);
-			movingDown=false;
-		}
-	}
-	return movingDown;
 }
 
 void Spatula::SetUpAuto(AutoInstructions instructions)
--- src/Spatula.h
+++ src/Spatula.h
@@ -8,32 +8,19 @@
 #ifndef SPATULA_H
 #define SPATULA_H
 
-#define SPATULA_UP 1
-#define SPATULA_DOWN 0
-
 #include "CANTalon.h"
 #include "MORESubsystem.h"
-#include "AnalogInput.h"
 
 class Spatula : public MORESubsystem
 {
 private:
 	CANTalon* rotaryMotor;
-	AnalogInput* pot;
-	bool movingUp;
-	bool movingDown;
 public:
 	static const int CURVE_IN = 1;
 	static const int CURVE_OUT = -1;
 
-	Spatula(int talonDeviceNumber, int potPort, string name);
+	Spatula(int talonDeviceNumber, string name);
 	~Spatula();
-
-	bool MoveUp();
-	bool MoveDown();
-
-	void StartMoveUp();
-	void StartMoveDown();
 
 	///Rotates the bottom of the lift
 	void Rotate(float speed);
