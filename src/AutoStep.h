636d9bb8d1a4fde5e6defceb75c40f57353ae24d
MORE Robotics Team 1714 <robotics1714@gmail.com>
2015-01-30 18:35:14 -0600

    Added comments for doxygen in the AutoStep and AutoTimer classes

--- src/AutoStep.h
+++ src/AutoStep.h
@@ -6,7 +6,11 @@
 
 using namespace std;
 
-//This class will be in charge of keeping track of one step of the autonomous routine
+/**
+ * This class keeps track of one step of an autonomous routine.
+ *
+ * The AutoStep class will take in instances of MORESubsystem and AutoInstructions and call the subsystem's Auto function.
+ */
 class AutoStep
 {
 private:
@@ -18,16 +22,44 @@ private:
 	bool firstLoop;//This variable keeps track of if it is the first time we are looping through this step
 	
 public:
+	/**
+	 * The constructor for the AutoStep class.
+	 *
+	 * @param subsystem An instance of MORESubsystem whose Auto() function is called to perform an action in autonomous.
+	 * @param instructions The instructions the Auto() function of the included MORESubstance will use to perform the correct autonomous action.
+	 * @param name The name of the AutoStep. The main purpose for the name parameter is debugging
+	 */
 	AutoStep(MORESubsystem* subsystem, AutoInstructions instructions, string name);//This class will require at least 1 set of subsystems and instructions
+	/**
+	 * The deconstructor for the AutoStep class.
+	 */
 	~AutoStep();
 	
-	//For when the auton routine requires two things to be happening at once
+	//For when the auto routine requires two things to be happening at once
+	/**
+	 * Adds another MORESubsystem and set of AutoInstructions to the AutoStep.
+	 *
+	 * This is used when a step in autonomous needs multiple subsystems to do things at once
+	 *
+	 * @param subsystem An instance of MORESubsystem whose Auto() function is called to perform in action in this autonomous step.
+	 * @param instructions The instructions the Auto() function of the included MORESubsystem will use to perform the correct autonomous action.
+	 */
 	void AddSubsystem(MORESubsystem* subsystem, AutoInstructions instructions);
 	
+	/**
+	 * Returns the name of the step
+	 *
+	 * @return the name of the step
+	 */
 	string GetStepName(){return stepName;}
 
-	//This function will be the one that actually performs the autonomous step
-	//It will return true if it is still performing and false when it is not
+	/*
+	 * This function will be the one that actually performs the autonomous step.
+	 *
+	 * It calls the Auto() of every MORESubsystem in this step.
+	 *
+	 * @return This function will return true if the step is still being performed. If the step has finished, the function will return false.
+	 */
 	bool PerformStep();
 };
 
--- src/AutoTimer.h
+++ src/AutoTimer.h
@@ -9,11 +9,37 @@ class AutoTimer : public MORESubsystem
 private:
 	Timer* clock;
 public:
+	/**
+	 * The constructor for the AutoTimer class
+	 *
+	 * @param name The name of the subsystem.
+	 */
 	AutoTimer(string name);
+	/**
+	 * The deconstructor for the AutoTimer class
+	 */
 	~AutoTimer();
 
-	void SetUpAuto(AutoInstructions instructions);
-	int Auto(AutoInstructions instructions);
+	/**
+	 * Will reset and start the clock.
+	 *
+	 * @param instructions The instructions that the function will use to perform the correct action
+	 * 	flags: none
+	 * 	param1: time to wait
+	 * 	param2 - param4: unused
+	 */
+	void SetUpAuto(AutoInstructions instructions) override;
+	/**
+	 * Performs the waiting action.
+	 *
+	 * @param instructions The instructions that the function will use to perform the correct action
+	 * 	flags: none
+	 * 	param1: time to wait
+	 * 	param2 - param4: unused
+	 *
+	 * @return Returns 1 if the time waited is less than the time param1 specified to wait. Returns 0 otherwise
+	 */
+	int Auto(AutoInstructions instructions) override;
 };
 
 
