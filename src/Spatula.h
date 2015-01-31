170e403d7e34b546a77406451ed9e00442eea405
MORE Robotics Team 1714 <robotics1714@gmail.com>
2015-01-30 19:23:38 -0600

    Added comments for doxygen in the Spatula class

--- src/Spatula.h
+++ src/Spatula.h
@@ -28,18 +28,51 @@ private:
 	bool movingUp;
 	bool movingDown;
 public:
+	/**
+	 * The constructor for the Spatula class
+	 *
+	 * @param talonDeviceNumber The CAN device ID number for the TalonSRX that will control the spatula
+	 * @param potPort The port of the potentiometer for the spatula
+	 * @param name The name of the subsystem
+	 */
 	Spatula(int talonDeviceNumber, int potPort, string name);
+	/**
+	 * The deconstructor for the Spatula class
+	 */
 	~Spatula();
 
+	/**
+	 * Moves the spatula to the all the way up position
+	 *
+	 * @return Returns true when the spatula is moving up. Returns false when it's done moving up.
+	 */
 	bool MoveUp();
+	/**
+	 * Moves the spatula to the all the way down position
+	 *
+	 * @return Returns true when the spatula is moving down. Returns false when it's done moving down.
+	 */
 	bool MoveDown();
 
+	/**
+	 * Allows the MoveUp function to start
+	 */
 	void StartMoveUp();
+	/**
+	 * Allows the MoveDown function to start
+	 */
 	void StartMoveDown();
 
-	///Rotates the bottom of the lift
+	/**
+	 * Rotates the spatula in or out at a specified speed
+	 *
+	 * @param speed The speed and direction to move the spatula
+	 */
 	void Rotate(float speed);
 
+	/**
+	 * @return Returns an instance of the potentiometer
+	 */
 	AnalogInput* GetPot(){return pot;}
 
 	///MORESubsystem Auto Functions
